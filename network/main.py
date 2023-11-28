4bc749843c5501e5aa92808c1e9432a0a86ed8e7
Seedname <45999377+Seedname@users.noreply.github.com>
2023-11-28 16:15:48 -0500

    b

--- network/main.py
+++ network/main.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 import multiprocessing as mp
 import time
 
+np.random.seed(42)
 
 np.set_printoptions(threshold=np.inf)
 
@@ -20,8 +21,8 @@ class Network:
     def __init__ (self, shape):
         self.shape = shape
 
-        self.weights = [np.random.randn(shape[i],shape[i-1]) for i in range(1, len(shape))]
-        self.biases =  [np.random.randn(shape[i], 1) for i in range(1, len(shape))]
+        self.weights = [np.sqrt(2/(shape[i-1] + shape[i])) * np.random.randn(shape[i],shape[i-1]) for i in range(1, len(shape))]
+        self.biases =  [0 * np.random.randn(shape[i], 1) for i in range(1, len(shape))]
 
     def feed_forward (self, input):
         activation = input
@@ -40,7 +41,7 @@ class Network:
             activations.append(np.mat(activation))
 
         return (activations, zs)
-
+    
     def backpropagation (self, input, target):
         activations, zs = self.feed_forward(input)
 
@@ -61,13 +62,14 @@ class Network:
         # queue.put((cost_gradient_weights, cost_gradient_biases))
         return (cost_gradient_weights, cost_gradient_biases)
 
-    def train (self, inputs, targets, epochs=30, mini_batch_size=64, learning_rate=0.01, patience=5, test_data=None):
+    def train (self, inputs, targets, epochs=30, mini_batch_size=64, learning_rate=0.01, patience=5, padding=0, test_data=None):
         training_data = list(zip(inputs, targets))
         patience_threshold = 2
         prev_accuracy = 0
         patience_counter = 0
         prev_time = time.time()
         y = []
+        y2 = []
 
         for full_pass in range(epochs):
             print(f'Epoch {full_pass+1}/{epochs}')
@@ -113,11 +115,29 @@ class Network:
 
             if test_data:
                 correct = 0
+                mse = None
                 for input, target in test_data:
-                    result, _ = neural_network.feed_forward(input)
+                    result, _ = self.feed_forward(input)
                     result = result[-1]
-                    if np.argmax(result) == np.argmax(target):
+                    
+                    if type(mse).__name__ == "NoneType":
+                        mse = np.zeros(result.T.shape)
+
+                    mse += np.square(target - result.T)
+
+                    if padding != 0:
+                        correct += 0.5
+                        # print(result, target)
+                        # if result[0]-padding <= target[0] and result[0]+padding >= target[0]:
+                            # correct += 1
+                    elif np.argmax(result) == np.argmax(target):
                         correct += 1
+
+                mse /= (len(test_data))
+                mse = np.mean(mse)
+
+                print(f"Loss: {mse}")
+                y2.append(mse)
 
                 accuracy = 100*correct/len(test_data)
                 print(f"Accuracy: {accuracy}%")
@@ -148,13 +168,74 @@ class Network:
         f = open('network/biases.txt', 'w')
         f.write(str(self.biases))
         f.close()
-        length = len(y)
+
         y.insert(0, 0)
-        plt.plot(list(range(0, length+1)), y)
+        plt.plot(list(range(0, len(y))), y)
         plt.xlabel("Epochs")
         plt.ylabel("Accuracy (%)")
         plt.title("Accuracy vs Epochs")
         plt.show()
+
+        plt.plot(list(range(0,len(y2))), y2)
+        plt.xlabel("Epochs")
+        plt.ylabel("Loss")
+        plt.title("Loss vs Epochs")
+        plt.show()
+
+def benchmark():
+    input_values_list = [[0,0], [0,1], [1,0], [1,1]]
+    target_values_list = [[1,0], [0,1], [0,1], [1,0]]
+
+    new_input = [np.mat(x).T for x in input_values_list]
+    new_target = [np.mat(x) for x in target_values_list]
+
+    test_data = list(zip(new_input, new_target))
+
+    times = 10
+    input_values = [x for x in new_input for _ in range(times)]
+    target_values = [x for x in new_target for _ in range(times)]
+
+    neural_network = Network([2, 3, 2])
+    neural_network.train(input_values, target_values, epochs=3000,  mini_batch_size=2000, learning_rate=0.8, patience=np.inf, test_data=test_data)
+
+def make_linear_regression(sets=1, points=1000, scale=10):
+    inputs = []
+    outputs = []
+
+    for _ in range(sets):
+        noise1 = list(np.random.randn(1, points))
+
+        m = np.random.randint(-100, 100)
+        b = np.random.randint(-100, 100)
+
+        pts = [[x, m * x + b + noise1[0][x] * scale] for x in range(points)]
+        
+        s_x = 0
+        s_y = 0
+        s_xx = 0
+        s_xy = 0
+
+        p = []
+
+        for x, y in pts:
+            s_x += x
+            s_y += y
+            s_xx += x*x
+            s_xy += x*y
+            p.append(x)
+            p.append(y)
+        
+        m = (points*s_xy - s_x*s_y)/(points*s_xx - s_x*s_x)
+        b = (s_y - m * s_x) / points
+        
+        input = np.asmatrix(p)
+        output = np.asmatrix([m, b])
+
+        inputs.append(input.T)
+        outputs.append(output)
+
+    return (inputs, outputs)
+
 
 if __name__ == "__main__":
     rows = []
@@ -179,19 +260,17 @@ if __name__ == "__main__":
     neural_network = Network([784, 32, 10])
     neural_network.train(input_values, target_values, epochs=50,  mini_batch_size=16, learning_rate=0.1, patience=5, test_data=test_data)
 
-    # input_values_list = [[0,0], [0,1], [1,0], [1,1]]
-    # target_values_list = [[1,0], [0,1], [0,1], [1,0]]
 
-    # new_input = [np.mat(x).T for x in input_values_list]
-    # new_target = [np.mat(x) for x in target_values_list]
+    # input_values, target_values = make_linear_regression(100, 500, 10)
+    # input_values_test, target_values_test = make_linear_regression(100, 500, 5)
 
-    # test_data = list(zip(new_input, new_target))
+    # test_data = list(zip(input_values_test, target_values_test))
 
-    # times = 10
-    # input_values = [x for x in new_input for _ in range(times)]
-    # target_values = [x for x in new_target for _ in range(times)]
+    # neural_network = Network([1000, 32, 2])
+    # neural_network.train(input_values, target_values, epochs=50,  mini_batch_size=16, learning_rate=0.8, patience=5, padding=2, test_data=test_data)
+    
 
-    # neural_network = Network([2, 3, 2])
-    # neural_network.train(input_values, target_values, epochs=300,  mini_batch_size=16, learning_rate=0.1, patience=np.inf, test_data=test_data)
+    # benchmark()
+
 
     
